413bcef34b3dc3f00418ba93f79abc452806ed43
shahbajnzminds <shahbaj.jamil@nzminds.com>
2026-01-20 22:35:04 +0530

    🚀 Web Release v1.0.6+1 (2026-01-20 22:35:04)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ea6a2f617ddbf2f563d29e61728c9546",
-"version.json": "4a7bc32a51edf6627779333f35f72720",
+const RESOURCES = {"flutter_bootstrap.js": "9e65d64af8bbebac32c4d58b22cf4d82",
+"version.json": "cbb61daaa3c262c2551bd598c4a27d50",
 "index.html": "abcd67ab0d09d0d5187db1c631227056",
 "/": "abcd67ab0d09d0d5187db1c631227056",
-"main.dart.js": "c447c8a45e434a6fe57bb08e496885e8",
+"main.dart.js": "dde4369171e90aebbe0c8fc1344d2f32",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "f3a8616a62c6bc9aec7aae3c601deb60",
 "icons/Icon-192.png": "6191ab197745fb50ab0d1fcbbbe39353",
