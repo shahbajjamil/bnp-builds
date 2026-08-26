20444312624a692d1c022ec1210e834a56beea47
shahbajjamil <shahbajjamil2014@gmail.com>
2026-08-26 17:31:27 +0530

    🚀 Web Release v2.2.1+16 (2026-08-26 17:31:27)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3969334330" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1088457970" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
